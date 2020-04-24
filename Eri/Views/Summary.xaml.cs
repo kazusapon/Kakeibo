75f94eaae7db3d6a28e42a297346b88e57cb2b89
kazusapon <flum1278pool@gmail.com>
2020-04-24 23:24:58 +0900

    サマリのビューを一部作成　まあそんなもんでしょう

---
+++ Eri/Views/Summary.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.StyleSheets;
+using Eri.Models;
+using Eri.Views;
+using Eri.ViewModels;
+
+
+namespace Eri.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class Summary : ContentPage
+    {
+        public Summary()
+        {
+            InitializeComponent();
+        }
+    }
+}
